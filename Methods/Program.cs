4950fc7dedc6cc561209c1a2480bdcee3ac7ce32
DevHobbyist <l3alazskiss15@gmail.com>
2022-10-03 11:45:22 -0300

    Update Program.cs

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,8 +8,25 @@ namespace Methods
 {
     internal class Program
     {
+        static int score;
         static void Main(string[] args)
         {
+            Console.WriteLine("Methods");
+
+            AddScore(0);
+            AddScore(300);
+            AddScore(400);
+
+            Console.ReadKey(true);
+        }
+        static void AddScore(int enemyPoints)
+        {
+            score = score + enemyPoints;
+            ShowHUD();
+        }
+        static void ShowHUD()
+        {
+            Console.WriteLine("Score: " + score);
         }
     }
 }
